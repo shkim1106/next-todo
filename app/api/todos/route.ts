323c7d00f10a52968cd10ad5db410cd17f36d1a3
shkim <shkim@pal-nat186-35-92.itap.purdue.edu>
2024-02-13 18:51:41 -0500

    plz

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -36,5 +36,5 @@ export async function POST(request: NextRequest) {
         data: addedTodo
     } 
 
-    return Response.json(response, { status: 201 });
+    return NextResponse.json(response, { status: 201 });
   }
