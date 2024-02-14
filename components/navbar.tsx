9701ae2542a51a9dcce2e1ca9d1e30b16ac71eb2
shkim <shkim0411064@naver.com>
2024-02-13 21:34:53 -0500

    commit 1

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -59,7 +59,7 @@ export const Navbar = () => {
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<FaApple />
-						<p className="font-bold text-inherit">SuHyuk's</p>
+						<p className="font-bold text-inherit">{`SuHyuk\'s`}</p>
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
