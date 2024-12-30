5c66cb5ba51e937d546cb5b34ed4b17e346c76f6
shkim <shkim0411064@naver.com>
2024-12-30 21:23:33 +0900

    d

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Use&nbsp;</h1>
+				{/* <h1 className={title()}>Use&nbsp;</h1>
 				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
+				<br /> */}
 				<h1 className={title()}>
 					Todo List made by SuHyuk
 				</h1>
--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,13 +1,10 @@
 import { title } from "@/components/primitives";
-import signInWithGoogle from "@/components/GoogleLogin"
 
 export default function PricingPage() {
 	return (
-		<><div>
-			<h1 className={title()}>Pricing</h1>
-		</div>
+		<>
 		<div>
-			<button onClick={signInWithGoogle}>Sign in With Google</button>
+			<h1 className={title()}>Pricing</h1>
 		</div>
 		</>
 	);
